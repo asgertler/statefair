7c2b6cbd9ccb9811e4cb24836eba6fa118e40a05
Aaron Gertler <aaron.gertler@gmail.com>
2020-09-04 10:38:28 -0500

    added food ticket button

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 // Import and invoke the ticket booth component function
 import { TicketBooth } from './TicketBooth.js'
-import { RideTicketHolders } from './rides/RideTicketHolders.js'
+import { RideTicketHolder } from './rides/RideTicketHolders.js'
+import { FoodTicketHolder } from './food/FoodTicketHolder.js'
 
 TicketBooth()
-RideTicketHolders()
+RideTicketHolder()
+FoodTicketHolder()
--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -1,7 +1,7 @@
 const contentTarget = document.querySelector(".rides")
 const eventHub = document.querySelector("#state-fair")
 
-export const RideTicketHolders = () => {
+export const RideTicketHolder = () => {
     eventHub.addEventListener("rideTicketPurchased", customEvent => {
         if ("rideId" in customEvent.detail) {
             contentTarget.innerHTML += `
